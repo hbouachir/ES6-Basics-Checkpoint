902a4f61b49419ecde725eba1c8532e68d10570b
hbouachir <bouachirhamza@gmail.com>
2020-05-17 12:47:18 +0100

    V0.1

---
+++ index.js
@@ -0,0 +1,22 @@
+var pets = [
+  { name: "Max", type: "dog", bornOn: 2018 },
+  { name: "Angel", type: "cat", bornOn: 2015 },
+  { name: "Jasper", type: "dog", bornOn: 2016 }
+];
+
+const getAge= (pet)=>{
+  return new Date().getFullYear() - pet.bornOn;
+}
+
+
+var petsWithAge = pets.map(p=>p.bornOn);
+ 
+console.log(petsWithAge);
+
+var dogs =pets.filter(p=>p.type==="dog");
+
+console.log(dogs);
+
+var jasper=pets.find(p=>p.name==="Jasper");
+
+console.log("Jasper is " + jasper.age + " years old");
